1a2b87d5384f56791b760aef03d9073616c9c3e7
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-02-17 15:38:28 +0100

    lesson

---
+++ script.js
@@ -0,0 +1,51 @@
+//VARIABLE: Global and Local Scope Variables
+ 
+//Global Variable
+
+let i;
+for (i= 0; i < 5; i++) {
+    let p = 10;
+    console.log(i+p);
+}
+
+// console.log(i);
+// // console.log(p); :-Local Variable
+
+//OPERATORS AND EQUALITY
+
+//Arithmetic Operators:-  +, -, /, %, ==, ===, Increment (++) & Decreament (--)
+
+// minus (-)
+let costi = 15;
+let profit;
+let salesPricei = 20;
+
+Profit = salesPricei - costi;
+
+console.log(Profit);
+
+// Plus (+) :-for NUmbers
+
+let cost = 15, profitt = 5, salesPrice = cost + profitt;
+console.log(salesPrice);
+
+// Plus (+) :-for Strings
+
+//Backtick ( ``):-
+let firstName = " Ola", surname = "Ade", fullName = surname + firstName;
+console.log(`${fullName} is a big boy`);
+
+
+let food = "Rice"
+
+console.log(`What will i eat if not ${food}?`);
+
+
+// The increment (++) and decrement (--) operators are used in JavaScript to increase or decrease the value of a variable by 1. Here's a real-life example of when you might use these operators: Let's say you have a variable called stockQuantity that represents the number of items in stock.
+
+let A = 2;
+ A *= 4;
+console.log(A);
+
+
+
