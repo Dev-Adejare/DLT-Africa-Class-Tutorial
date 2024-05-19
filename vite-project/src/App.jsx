b6a7de482c88f5b2f234ffe547859b67e0c40e3f
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-19 14:55:25 +0100

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
   const [names, setNames] = useState(["Ade", "Yemi", "Aderayo"]);
+  const [user, setUser] = useState("");
 
   const handleIncrement = () => {
     setCount(count + 1);
@@ -18,6 +19,14 @@ function App() {
   const handlereset = () => {
     setCount(0);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setNames(prev => [...prev, user])
+  };
+
+
+
   return (
     <>
       <h1>Count: {count}</h1>
@@ -26,16 +35,22 @@ function App() {
       <button onClick={handleIncrementBy5}>handleDecrementBy5</button>
       <button onClick={handlereset}>reset</button>
 
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        value={user}
+        onChange={(e) => setUser(e.target.value)}
+        />
+        
+        <button type="submit">Add Name</button>
+        <button type="reset">Reset Input</button>
+      </form>
+      
       {names.map((name, index, arr) => (
        <div><p key={index}>Name: {name}</p>
          <p> Index of {index}</p>
         </div> 
       ))}
 
-      <form action="">
-        <input type="text" />
-        <button type="submit">Add Name</button>
-      </form>
     </>
   );
 }
