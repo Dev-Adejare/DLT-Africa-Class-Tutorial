75fc3bb0ec4157ea0b8edf063076b45588030ea1
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-19 14:35:33 +0100

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,32 +1,42 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [names, setNames] = useState(["Ade", "Yemi", "Aderayo"]);
 
-    const handleIncrement = () => {
-      setCount(count + 1)
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleIncrementBy5 = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prev) => prev + 1);
     }
-    const handleIncrementBy5 = () => {
-      for (let i = 0; i < 5; i++) {
-
-        setCount((prev) => prev + 1)
-      }
-    }
-    const handleDecrement = () => {
-      setCount(count - 1);
-    }
-    const handlereset = () => {
-      setCount(0);
-    }
-    return (
-      <>
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  const handlereset = () => {
+    setCount(0);
+  };
+  return (
+    <>
       <h1>Count: {count}</h1>
       <button onClick={handleIncrement}>increment</button>
       <button onClick={handleDecrement}>decrement</button>
       <button onClick={handleIncrementBy5}>handleDecrementBy5</button>
       <button onClick={handlereset}>reset</button>
-      </>
-    );
+
+      {names.map((name, index, arr) => (
+       <div><p key={index}>Name: {name}</p>
+         <p> Index of {index}</p>
+        </div> 
+      ))}
+
+      <form action="">
+        <input type="text" />
+        <button type="submit">Add Name</button>
+      </form>
+    </>
+  );
 }
-export default App
+export default App;
