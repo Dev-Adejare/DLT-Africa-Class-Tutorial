0ce4329a30466b504fce2e5ba83ff27cd9be991a
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-19 14:16:48 +0100

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,15 +7,26 @@ function App() {
     const handleIncrement = () => {
       setCount(count + 1)
     }
+    const handleIncrementBy5 = () => {
+      for (let i = 0; i < 5; i++) {
+
+        setCount((prev) => prev + 1)
+      }
+    }
     const handleDecrement = () => {
       setCount(count - 1);
     }
+    const handlereset = () => {
+      setCount(0);
+    }
     return (
       <>
-      <h1>{count}</h1>
+      <h1>Count: {count}</h1>
       <button onClick={handleIncrement}>increment</button>
-      <button onClick={handleDecrement}>increment</button>
+      <button onClick={handleDecrement}>decrement</button>
+      <button onClick={handleIncrementBy5}>handleDecrementBy5</button>
+      <button onClick={handlereset}>reset</button>
       </>
-    )
+    );
 }
 export default App
